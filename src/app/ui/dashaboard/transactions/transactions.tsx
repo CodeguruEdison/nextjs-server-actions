6116d9fdfda785f073f6efb4fe4347cfdad806c5
Ayaskanta <silu.samal@gmail.com>
2025-02-23 07:45:31 -0500

    Add chart component with data visualization and update transactions layout

--- src/app/ui/dashaboard/transactions/transactions.tsx
+++ src/app/ui/dashaboard/transactions/transactions.tsx
@@ -5,50 +5,101 @@ import Image from 'next/image'
 export const Transactions = (props: ITransactionsProps) => {
     return (
         <div className={styles.container}>
-            <h2 className={styles.title}> Latest Transaction</h2>
+            <h2 className={styles.title}>Latest Transactions</h2>
             <table className={styles.table}>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Date</th>
-                        <th>Amount</th>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                        <td>Amount</td>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>
                             <div className={styles.user}>
-                                <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} /> John Doe
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
                             </div>
                         </td>
-                        <td><span className={` ${styles.status} ${styles.pending}`}>Pending</span></td>
-                        <td><span >14.02.2024</span></td>
-                        <td><span >$3.2000</span></td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>
+                                Pending
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
                     </tr>
                     <tr>
                         <td>
                             <div className={styles.user}>
-                                <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} /> John Doe
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
                             </div>
                         </td>
-                        <td><span className={` ${styles.status} ${styles.done}`}>Done</span></td>
-                        <td><span >14.02.2024</span></td>
-                        <td><span >$3.2000</span></td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>Done</span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
                     </tr>
                     <tr>
                         <td>
                             <div className={styles.user}>
-                                <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} /> John Doe
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
                             </div>
                         </td>
-                        <td><span className={` ${styles.status} ${styles.cancelled}`}>Done</span></td>
-                        <td><span >14.02.2024</span></td>
-                        <td><span >$3.2000</span></td>
+                        <td>
+                            <span className={`${styles.status} ${styles.cancelled}`}>
+                                Cancelled
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>
+                                Pending
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
                     </tr>
                 </tbody>
             </table>
-
         </div>
-    )
+    );
 }
